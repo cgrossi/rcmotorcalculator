aa041ed931c8b934f8a3c0bc054d6ffec1ad0437
cgrossi <cgrossi@gmail.com>
2018-04-24 15:36:48 -0600

    add javascript

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-var input1 = $("#input1").bind("keyup mouseup", function(){
-	console.log($(this).val());
-})
-var output1 = $("#output1").text();
-
-
-function output1(){
-	
+function calculate(){
+	var input1 = document.getElementById("input1").value;
+	var input2 = document.getElementById("input2").value;
+	var input3 = document.getElementById("input3").value;
+  
+	document.getElementById("output1").innerText = input1;
+  document.getElementById("output2").innerText = input2;
+  document.getElementById("output3").innerText = input3;
 }
